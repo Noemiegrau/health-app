50b2b93b9164deadd4d40e6b8d9c4ced6e0ee872
Noemiegrau <noemiegrau@outlook.fr>
2021-11-12 22:13:32 -0800

    try to push client folder

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import TaskList from "./Components/taskList.js";
+
+function App() {
+  return (
+    <div className="App">
+      <TaskList />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      {/* <h1>CRUD APPLICATION</h1>
+
+    <div className="form">
+      <label>Movie Name:</label>
+      <input type="text" name="movieName" />
+      <label>Review:</label>
+      <input type="text" name="review" />
+
+      <button>Submit</button>
+      </div> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/taskList.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import axios from 'axios'; 
+
+class TaskList extends React.Component {
+    state = {
+        task: "", // current user entered task
+        taskList: [] //holds all the tasks
+    };
+
+    componentDidMount(){
+        this.getTaskList();
+    };
+ 
+    getTaskList = () => {
+        axios.get('http://localhost:4000/tasks')
+        .then((response) => response.data)
+        .then((response => this.setState({taskList: response})));
+    };
+
+    onDeleteClick = id => {
+        axios.delete(`http://localhost:4000/deleteTask/${id}`);
+        this.getTaskList();
+    };
+
+    onSubmitClick = () => {
+        axios.post('http://localhost:4000/addTask', {
+            task: this.state.task
+        });
+        this.getTaskList();
+        this.setState({task: ''})
+    };
+
+
+    render() {
+        console.log(this.state.id);
+        return (
+            <div>
+                <h3>TaskList</h3>
+                <div className="ui input">
+                    <input value={this.state.task} onChange={e => this.setState({
+                        task: e.target.value
+                    })} placeholder="your tasks.." />
+                </div>
+                <button className="ui primary button basic" onClick={() => this.onSubmitClick()}>Submit</button>
+                <hr />
+                <div className="ui cards">
+                    {this.state.taskList.map((task) => (
+                    <div className="card">
+                        <div className="content">
+                            <div className="meta">
+                                {task.task_name}
+                            </div>
+                            <div className="extra content">
+                                <div className="ui two buttons">
+                                    <div className="ui two buttons">
+                                        <button className="ui basic green button">Done</button>
+                                        <button className="ui basic red button" onClick={() => this.onDeleteClick(task.id)}>Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TaskList;
