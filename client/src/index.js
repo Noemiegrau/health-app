39b217f23e946193bafc677abd86b15abeb8be60
Agnes <agnes.tshibangugrillon@gmail.com>
2021-12-07 00:09:09 +0100

    about page and signup function

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import Login from './pages/Login'
 import Todolist from './pages/Todolist'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
+import LoginSignup from './Components/Login-signup'
+
+
 import './style/index.scss'
 
 
@@ -19,6 +22,8 @@ ReactDOM.render(
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />  
       <Route path="/todolist" element={<Todolist />} />  
+      <Route path="/login-signup" element={<LoginSignup />} />
+
     </Routes>
     <Footer /> 
   </Router>,
@@ -28,22 +33,3 @@ ReactDOM.render(
 
 
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter } from "react-router-dom";
-
-// import App from './App';
-// // import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
-
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// // reportWebVitals();
